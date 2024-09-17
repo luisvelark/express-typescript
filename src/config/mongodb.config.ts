f0bd32f31273ce64ab873228718317c1094779b9
velark <velark07@gmail.com>
2024-09-16 23:51:13 -0500

    feat: conexion a mongodb

--- src/config/mongodb.config.ts
+++ src/config/mongodb.config.ts
@@ -4,7 +4,6 @@ import mongoose, { connect } from 'mongoose'
 async function dbConnect (): Promise<void> {
   mongoose.set('strictQuery', false)
   const DB_URI = process.env.DB_URI as string
-  // const DB_URI = 'mongodb://127.0.0.1:27017/api-rest-ts'
   await connect(DB_URI)
 }
 
